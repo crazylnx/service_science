481448686a733ea777f29f8847514735b754920b
crazylnx <741558515@163.com>
2019-01-03 19:05:24 +0800

    完成侧边栏的top3问题

--- log_in/models.py
+++ log_in/models.py
@@ -4,7 +4,7 @@ class user(models.Model):
     user_name = models.CharField(max_length=8)
     user_password = models.CharField(max_length=10)
     user_money = models.IntegerField(default=0)
-    user_image = models.CharField(default="", max_length=200)
+    user_image = models.CharField(  default='', max_length=200)
     user_nickname = models.CharField(max_length=6)
     def __str__(self):
         return self.user_name
